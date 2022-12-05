67f88500143f2f9b4a0905573a02522dcf510f7c
Lubna Tahreem <lubnatahreem789@gmail.com>
2022-12-05 11:05:42 +0530

    creatd a file for contact chat list

--- src/components/Contactlogs.jsx
+++ src/components/Contactlogs.jsx
@@ -5,12 +5,14 @@ import MessageIcon from '@mui/icons-material/Message';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SearchIcon from '@mui/icons-material/Search';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import Search from '@mui/icons-material/Search';
+import Contactchatlogs from './Contactchatlogs';
 
 function Contactlogs() {
   return (
     <div className='bg-grey-900 h-screen'>
       {/* 1st contact header section */}
-        <div className='flex text-black justify-between items-center px-4 py-2 bg-slate-800'>
+        <div className='flex text-black justify-between items-center px-4 py-2 bg-slate-700'>
           <div> 
             <img className='h-8 w-8 rounded-full' src='https://i.pinimg.com/originals/fe/65/be/fe65bed80b33375458e7c307aa28859d.png' alt=''/>
             </div>
@@ -23,14 +25,19 @@ function Contactlogs() {
         </div>
 
         {/* 2nd search section */}
-        <div className='bg-pink-500 justify-between'>
-          <div className='bg-slate-800'>
-            <SearchIcon/>
-            <input type={text} name="search" value="Search or Start new chat"/>
+        <div className='flex mx-2 my-2 text-lg text-slate-300 space-x-2 justify-between items-center'>
+          <div className='bg-slate-600  space-x-2 px-2 items-center rounded-lg w-full'>
+            <SearchIcon fontSize='inherit'/>
+            <input className='bg-transparent text-xs' type={Search} name="search" placeholder="Search or Start new chat"/>
           </div>
           <div>
-            <KeyboardDoubleArrowDownIcon/>
+            <KeyboardDoubleArrowDownIcon  fontSize='inherit'/>
           </div>
+        </div>
+
+        {/* contact chat logs */}
+        <div>
+          <Contactchatlogs/>
         </div>
         
     </div>
