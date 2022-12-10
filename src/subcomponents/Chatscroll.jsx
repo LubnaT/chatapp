b6e5233231f6e620b284946e143564483063a2ea
Lubna Tahreem <lubnatahreem789@gmail.com>
2022-12-10 23:01:16 +0530

    changing the scorll code for scrolling

--- src/subcomponents/Chatscroll.jsx
+++ src/subcomponents/Chatscroll.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Message(props){
 return (
    <div className=''>
-    {props.me ? <div className='w-full flex justify-end'>
+    {props.me ? <div className='w-full max-h-max flex justify-end'>
         <div className='max-w-max px-2 text-sm py-1 rounded-br-none rounded-lg shadow-md bg-white text-gray-800'>
         <p>{props.text}</p>
     </div>
@@ -18,9 +18,26 @@ return (
 function Chatscroll() {
 
   return (
-    <div className='text-white w-full h-full py-16 px-8 space-y-4 justify-end flex flex-col'>
+    <div>
+      <div className='text-white w-full h-full py-16 px-8 space-y-4 justify-end flex flex-col'>
         {/* messages sent by me nd other */}
         {/* Message component attribute text is msg and me is whether msg is sent by me or others */}
+        <Message text="Hi" me={true}/>
+        <Message text="How r u??" me={true}/>
+        <Message text="Hello" me={false}/>
+        <Message text="iam fine" me={false}/>
+        <Message text="what about u?" me={false}/>
+        <Message text="Good" me={true}/>
+        <Message text="how is was the day?" me={true}/>
+        <Message text="nice" me={false}/>
+        <Message text="Hi" me={true}/>
+        <Message text="How r u??" me={true}/>
+        <Message text="Hello" me={false}/>
+        <Message text="iam fine" me={false}/>
+        <Message text="what about u?" me={false}/>
+        <Message text="Good" me={true}/>
+        <Message text="how is was the day?" me={true}/>
+        <Message text="nice" me={false}/>
         <Message text="Hi" me={true}/>
         <Message text="How r u??" me={true}/>
         <Message text="Hello" me={false}/>
@@ -33,6 +50,9 @@ function Chatscroll() {
         {/* empty div for giving space to msg from bottom */}
         <div className='h-16'> </div>
     </div>
+
+    </div>
+    
   )
 }
 
