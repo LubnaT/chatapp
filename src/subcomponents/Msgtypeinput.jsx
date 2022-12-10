7ace4b410b90f015621007bf0371bf50f357b27e
Lubna Tahreem <lubnatahreem789@gmail.com>
2022-12-10 22:56:18 +0530

    creating handle function

--- src/subcomponents/Msgtypeinput.jsx
+++ src/subcomponents/Msgtypeinput.jsx
@@ -5,11 +5,18 @@ import Search from '@mui/icons-material/Search';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
 
 function Msgtypeinput() {
+   // console.log(props.msgSetter)
+
   return (
     <div className='p-2  space-x-2 bg-slate-700 '>
         <InsertEmoticonIcon/>
         <AttachFileIcon className='rotate-45'/>
-        <input className='bg-slate-600 rounded-md pl-2 text-xs w-[720px] h-[25px] shadow-2xl' type={Search} name="search" placeholder="Type a message"/>
+        <input 
+        name="myMessage"
+        value={text.myMessage}
+        onChange={handleChange}
+        placeholder="Type a Message"
+        className='bg-slate-600 rounded-md pl-2 text-xs w-[720px] h-[25px] shadow-2xl' type={Search} name="search" placeholder="Type a message"/>
         <KeyboardVoiceIcon/>
     </div>
   )
