f68c07f6f97bbacb72c86e748029b65125b279c1
Lubna Tahreem <lubnatahreem789@gmail.com>
2022-12-12 18:25:31 +0530

    changed login page

--- src/screens/Loginpage.jsx
+++ src/screens/Loginpage.jsx
@@ -2,20 +2,55 @@ import React from 'react'
 
 function Loginpage(props) {
   return (
-    <div>
-         <div id="login-area" class="mt-10">
-        <h1 class="text-center font-bold text-5xl italic">Whatsapp Login</h1>
-        <div id="login" class="bg-[#EAEDED] rounded-lg p-10 shadow-2xl w-[1000px] h-[300px] mt-[100px] ml-[200px]">
-            <h3 class="text-2xl drop-shadow-xl font-semibold ml-[40px]">Login</h3>
-            <input id="email" type="text" class="mt-[10px] pointer-events-auto cursor-text border-solid border-2 border-zinc-600 h-10 w-[500px] ml-[20px]" placeholder="Email"/>
-            <br/>
-            <input id="password" type="text" class="mt-[10px] cursor-text h-10 w-[500px] ml-[20px] border-solid border-2 border-zinc-600" placeholder="Password"/>
-            <br/>
-            <button id="submit-btn" onClick="valid()" class=" hover:bg-red-400 rounded-md text-center mt-[30px] ml-[20px] border-solid border-2 border-black text-lg shadow-2xl bg-indigo-700 p-2 h-12 w-36">Submit</button>
-            <button id="submit-btn" onClick={props.login} class=" rounded-md text-center mt-[40px] ml-[20px] border-solid border-2 border-black text-lg shadow-2xl bg-white p-2 h-12 w-36">Google</button>
-        </div>
-    </div>
+    
 
+    <div className=" h-screen w-full overflow-x-hidden bg-white grid grid-col-1 lg:grid-cols-2">
+      <div className="ml-[380px]">
+        <div className=" px-10 pt-4">
+        <div className=" flex items-center pt-1">
+          {/* <img className=" h-14 w-14" src="" alt="LOGO of Site" /> */}
+          {/* name of chat website */}
+          <p className=" text-2xl font-medium text-gray-800 ">Easy Chat</p>
+        </div>
+        <div className=" pt-10">
+            <p className="text-2xl font-medium text-gray-700 mb-4">Log in</p>
+            <div className=" flex space-x-3">
+                <button onClick={()=> props.login(true)} className=" flex items-center space-x-1 px-4 border py-[6px] w-1/2 justify-center rounded-md">
+                    <img alt="google-oauth2" className=" h-5 w-5" src="//heathmont.imgix.net/bitcasino/images/google-oauth2.svg?auto=compress,format" loading="lazy" />
+                        <span>Google</span>
+                </button>
+            </div>
+
+        </div>
+        <div className=" flex items-center space-x-2 my-4 ">
+            <hr className=" border w-full" />
+            <p className=" text-sm font-medium text-gray-600">or</p>
+            <hr className=" border w-full" />
+
+        </div>
+        <div className=" flex flex-col  space-y-3">
+            <div className=" flex flex-col space-y-2">
+                <p className=" font-ligh text-gray-600 ">Email</p>
+                <input className=" border rounded-md py-2" />
+            </div>
+            <div className=" flex flex-col space-y-2">
+                <p className=" font-ligh text-gray-600 ">Password</p>
+                <input className=" border rounded-md py-2" />
+            </div>            
+        </div>
+        <div className=" mt-6">
+            <button className=" py-3 rounded-md font-bold  bg-fuchsia-500 text-white w-full "> Log in</button>
+        </div>
+        <div className=" text-purple-500 pt-4 flex flex-col space-y-1">
+            <p className=" text-sm font-medium cursor-pointer">Forgot Password?
+            </p>
+            
+            <p className=" text-sm font-medium text-gray-800"> {`Don't have an account ?`} <span className=" text-sm font-medium hover:underline cursor-pointer text-purple-500">Create one</span></p>
+            <p className=" leading-4 text-xs text-gray-500 pt-6">This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
+        </div>
+        </div>
+      </div>
+      
     </div>
   )
 }
