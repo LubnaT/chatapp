340f2fa9f469be37c9000d273541614751cfa0e8
Lubna Tahreem <lubnatahreem789@gmail.com>
2022-12-12 18:14:52 +0530

    created spme more comments

--- src/subcomponents/Msgtypeinput.jsx
+++ src/subcomponents/Msgtypeinput.jsx
@@ -14,9 +14,16 @@ function Msgtypeinput() {
   }
   const [text , setText] = useState(schema)
 
+  // event is object which is passed to fntn and contains input details like name,value etc 
+  // whatever input contains 
   function handleChange(event){
-    console.log(event)
+    // console.log(event)
+  //  value is input we type as a single value of all letter 
+  // setting value to event.target where target is object inside a event object
+  // target contains the values of input tag
     const {value} = event.target ;
+    // uid to know user log in nd sending msg is same 
+    // this fntn display msg with uid
     setText({uid : `12345` , myMessage : value})
     
 
@@ -28,8 +35,10 @@ function Msgtypeinput() {
         <input 
           // name should be same as myMessage key which stores messages
         name="myMessage"
-        // text is setter variable which is whatever key we are typing is passed to myMessage key
+        // text is setter variable which is whatever key or each letter we are typing is passed
+        // is passed as seperate value to myMessage key
         value={text.myMessage}
+        // whenever there is change in input or leeter typing in input it call handleChange fntn
         onChange={handleChange}
         placeholder="Type a Message"
         className='bg-slate-600 rounded-md pl-2 text-xs w-[720px] h-[25px] shadow-2xl' 
