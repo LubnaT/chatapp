5e45a7875a36ac8b48d6377619ce105df35fa826
Lubna Tahreem <lubnatahreem789@gmail.com>
2022-12-21 22:41:23 +0530

    changed the css

--- src/subcomponents/Loder.jsx
+++ src/subcomponents/Loder.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Loder() {
   return (
-    <div class="h-screen flex justify-center items-center bg-black">
+    <div class="h-screen bg-black">
         <div  className='relative'>
         <img className='h-screen w-screen'
         src='https://i0.wp.com/codemyui.com/wp-content/uploads/2017/09/rotate-pulsating-loading-animation.gif?fit=880%2C440&ssl=1'/>
