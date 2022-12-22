38a0f51fe7c5040dc70b9ad58bf9cececb1cc979
Lubna Tahreem <lubnatahreem789@gmail.com>
2022-12-22 17:49:03 +0530

    creating user db

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -5,6 +5,7 @@ import Msgtypeinput from '../subcomponents/Msgtypeinput'
 
 
 function Chats(props) {
+  const [messageArray ,setMessageArray] = useState([]) ;
   return (
     <div className=' col-span-3 relative h-screen  '>
         {/*Header */}
@@ -17,11 +18,11 @@ function Chats(props) {
         {/* instead of padding cant we take margin bcz there are its taking 
         padding according to content */}
         <div className='h-screen banner overflow-y-scroll scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-white scrollbar-track-blue-200 scrollbar-thin'>  
-           <Chatscroll/>
+           <Chatscroll  message={messageArray}/>
         </div>
         {/* chat type */}
         <div className='absolute bottom-0 z-10 bg-orange-500 w-full'> 
-        <Msgtypeinput/>
+        <Msgtypeinput msgSetter={setMessageArray}/>
         </div>
     </div>
   )
